24739884858d59e4a80b0815cab52507e22accdb
권용근 <kingbbode@gmail.com>
2017-06-04 22:02:08 +0900

    sample package 작성

---
+++ src/gs1/kingbbode/App.java
@@ -0,0 +1,10 @@
+package gs1.kingbbode;
+
+/**
+ * Created by YG-MAC on 2017. 6. 4..
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("sample");
+    }
+}
