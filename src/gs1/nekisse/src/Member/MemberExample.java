9d700a593a090d5bbff9b79c79a10923ded4efa2
Nekisse <lsh891224@gmail.com>
2017-06-06 17:43:31 +0900

    조회메소드추가
    조회메소드

--- src/gs1/nekisse/src/Member/MemberExample.java
+++ src/gs1/nekisse/src/Member/MemberExample.java
@@ -12,31 +12,66 @@ public class MemberExample {
     boolean start = true;
 
     public void run(){
+        while (start) {
+            System.out.println("----------------------------------------");
+            System.out.println("1.사원생성 | 2.사원수정 | 3.사원삭제 | 4.사원조회");
+            System.out.println("----------------------------------------");
+            System.out.print("선택> ");
+
         int munuNum = scanner.nextInt();
 
-        switch (munuNum){
-            case 1:
-                 addmember(); break;
-//            case 2:
-//                modmember(); break;
-//            case 3:
-//                delmember(); break;
-//            case 4:
-//                searchmember(); break;
-        }
 
+
+          switch (munuNum) {
+              case 1:
+                  addmember();
+                  break;
+            case 2:
+                modmember(); break;
+            case 3:
+                delmember(); break;
+              case 4:
+                  searchmember();
+                  break;
+          }
+      }
 
     }
+
+
+
 
     public void addmember() {
         String name="";
         scanner.nextLine();
         System.out.println("직원 이름 : ex)홍길동  ");
-        Member member = new Member(name);
         name = scanner.nextLine();
-        people.add(member);
+        people.add(name);
 
-        System.out.println(people.get(0)+ " 생성하였습니다.");
+        System.out.println(name+ " 생성하였습니다.");
+    }
+
+    public void modmember() {
+    }
+
+
+    public void searchmember() {
+        for (int i = 0; i < people.size(); i++) {
+            String Allpeople = people.get(i);
+            System.out.println("모든 사원을 조회합니다");
+            System.out.println(i+ "번째 사원 : " + Allpeople);
+        }
+    }
+
+
+    public void delmember() {
+        String name="";
+        scanner.nextLine();
+        System.out.println("삭제할 직원 이름 : ex)홍길동 ");
+        name = scanner.nextLine();
+        people.remove(name);
+
+
     }
 
 
