a6ba145ab2904a70b6b5858fc83b35c5bef15f68
minomi <5minhominho@gmail.com>
2017-06-07 12:23:30 +0900

    package 정의

---
+++ src/gs1/minho/view/console/Console.java
@@ -0,0 +1,14 @@
+package gs1.minho.view.console;
+
+import java.util.Scanner;
+
+/**
+ * Created by minho on 2017. 6. 7..
+ */
+
+public class Console {
+    Scanner scanner;
+    public Console() {
+        scanner = new Scanner(System.in);
+    }
+}
