c8c10d0246e596abd57535bb00f41b2e8ccda651
minomi <5minhominho@gmail.com>
2017-06-07 12:22:24 +0900

    패키지 정의

---
+++ src/gs1/minho/EmplyeeManager.java
@@ -0,0 +1,16 @@
+package gs1.minho;
+
+
+/**
+ * Created by minho on 2017. 6. 7..
+ */
+public class EmplyeeManager {
+    public static void main(String[] args) {
+        EmplyeeManager emplyeeManager = new EmplyeeManager();
+        emplyeeManager.run();
+    }
+
+    private void run() {
+        System.out.print("start");
+    }
+}
