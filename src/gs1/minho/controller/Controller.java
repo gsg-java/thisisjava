7d8df4cde992e0806d4c3c76550c6848d7798d45
minomi <5minhominho@gmail.com>
2017-06-08 19:52:35 +0900

    오타 수정

--- src/gs1/minho/controller/Controller.java
+++ src/gs1/minho/controller/Controller.java
@@ -4,7 +4,6 @@ import gs1.minho.annotation.RequestView;
 import gs1.minho.annotation.ResponseView;
 import gs1.minho.annotation.Service;
 import gs1.minho.exception.AlreadyExistingEmployeeException;
-import gs1.minho.model.Emplyee;
 import gs1.minho.request.Request;
 import gs1.minho.service.EmployeeService;
 import gs1.minho.view.EmployeeView;
@@ -18,19 +17,20 @@ import java.lang.reflect.Method;
  * Created by minho on 2017. 6. 7..
  */
 
+//컨트롤러는 뷰와 서비스 객체의 메서드 테이블을 가짐
 public class Controller {
     private EmployeeView view;
     private EmployeeService service;
     private Map<String, Method> serviceMethodTable;
     private Method[] requestViewMethodTable;
-    private Map<String, Method> resposseViewMethodTable;
+    private Map<String, Method> responseViewMethodTable;
 
     public Controller() {
         view = new EmployeeView();
         service = new EmployeeService();
         serviceMethodTable = new HashMap<>();
         requestViewMethodTable = new Method[EmployeeView.class.getDeclaredMethods().length];
-        resposseViewMethodTable = new HashMap<>();
+        responseViewMethodTable = new HashMap<>();
 
         for(Method method : EmployeeView.class.getDeclaredMethods()) {
             if(method.isAnnotationPresent(RequestView.class)) {
@@ -40,7 +40,7 @@ public class Controller {
 
         for(Method method : EmployeeView.class.getDeclaredMethods()) {
             if(method.isAnnotationPresent(ResponseView.class)) {
-                resposseViewMethodTable.put(method.getAnnotation(ResponseView.class).request(), method);
+                responseViewMethodTable.put(method.getAnnotation(ResponseView.class).request(), method);
             }
         }
 
@@ -56,7 +56,6 @@ public class Controller {
             int selected = view.showMenu();
             try {
                 Object request = requestViewMethodTable[selected].invoke(view);
-//                System.out.print(((Request)request).getRequestName());
                 processRequest((Request) request);
             } catch (Exception e) {
 
@@ -67,10 +66,9 @@ public class Controller {
     private void processRequest(Request request) {
         try {
             String requestName = request.getRequestName();
-//            System.out.print(requestName);
             Object object = serviceMethodTable.get(requestName).invoke(service, request);
             if(object != null) {
-                resposseViewMethodTable.get(requestName).invoke(view, object);
+                responseViewMethodTable.get(requestName).invoke(view, object);
             }
         } catch (AlreadyExistingEmployeeException e) {
         } catch (Exception e) {
