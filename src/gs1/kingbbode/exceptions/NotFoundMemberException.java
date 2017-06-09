996cda33ad52483388fef99aff85a682c8226bf2
권용근 <kingbbode@gmail.com>
2017-06-09 23:43:36 +0900

    exception 정의

---
+++ src/gs1/kingbbode/exceptions/NotFoundMemberException.java
@@ -0,0 +1,10 @@
+package gs1.kingbbode.exceptions;
+
+/**
+ * Created by YG-MAC on 2017. 6. 9..
+ */
+public class NotFoundMemberException extends CommandException{
+    public NotFoundMemberException() {
+        super("존재하지 않는 사원입니다.");
+    }
+}
