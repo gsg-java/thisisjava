daca561ed1f7e90c65ca66e81172cd2dc903ce06
minomi <5minhominho@gmail.com>
2017-06-12 14:34:13 +0900

    CompanyPosition에 String position 변수 추가

--- src/gs1/minho/enums/CompanyPosition.java
+++ src/gs1/minho/enums/CompanyPosition.java
@@ -5,5 +5,16 @@ package gs1.minho.enums;
  * Created by minho on 2017. 6. 8..
  */
 public enum CompanyPosition {
-    CLERK, SENIOR, ASSIST, MANAGER
+    CLERK("사원"), SENIOR("주임"), ASSIST("대리"), MANAGER("과장");
+
+    private String position;
+
+    CompanyPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return position;
+    }
 }
