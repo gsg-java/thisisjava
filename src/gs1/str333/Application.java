82250f3f9f0339bb8f6cc1128223963e8ab36567
이은지 <str7mscmp@gmail.com>
2017-06-17 03:49:09 +0900

    main

---
+++ src/gs1/str333/Application.java
@@ -0,0 +1,11 @@
+package gs1.str333;
+
+/**
+ * @author str333
+ * @date 2017. 6. 11.
+ */
+public class Application {
+	public static void main(String[] args) {
+		new ManageForm().getManageForm();
+	}
+}
