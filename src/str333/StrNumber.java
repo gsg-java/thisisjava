a7e96718e657f9df1fbf5c1530c0647b0f51d9dd
이은지 <str7mscmp@gmail.com>
2017-07-15 07:26:34 +0900

    StrNumber 인터페이스 생성

---
+++ src/str333/StrNumber.java
@@ -0,0 +1,10 @@
+package str333;
+
+public interface StrNumber {
+	public int getIntNumber(); 
+	public double getDoubleNumber(); 
+	public StrNumber sum(StrNumber strNumber);
+	public StrNumber sub(StrNumber strNumber);
+	public StrNumber mul(StrNumber strNumber);
+	public StrNumber div(StrNumber strNumber);
+}
