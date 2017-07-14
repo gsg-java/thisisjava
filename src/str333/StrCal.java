c3f3a3eef731c82b8773b82edf491ebfdecaf06f
이은지 <str7mscmp@gmail.com>
2017-07-15 07:27:52 +0900

    계산 테스트 - main

---
+++ src/str333/StrCal.java
@@ -0,0 +1,11 @@
+package str333;
+
+public class StrCal {
+	public static void main(String[] args) {
+
+        System.out.println(new StrInteger(10).sum(new StrInteger(20)).getIntNumber());
+        System.out.println(new StrDouble(12.4).sub(new StrInteger(20)).getDoubleNumber());
+        System.out.println(new StrInteger(11).mul(new StrInteger(20)).getIntNumber());
+        System.out.println(new StrDouble(44).div(new StrDouble(20)).getDoubleNumber());
+	}
+}
